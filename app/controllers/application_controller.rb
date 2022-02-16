6c0550cfe59c714013d19171dc89c63f6f7590f6
EC2 Default User <ec2-user@ip-172-31-83-143.ec2.internal>
2022-02-16 14:28:54 +0000

    [add]page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    public_customers_my_page_path(resource)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,
+                                                         :first_name,
+                                                         :last_name_kana,
+                                                         :first_name_kana,
+                                                         :postal_code,
+                                                         :address,
+                                                         :telephone_number])
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "adomin/sessions"
+  }
+
+  root to: "homes#top"
+  get '/about' => 'homes#about'
+  resources :items, only: [:index, :show]
+
+  namespace :public do
+    root to: 'homes#top'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+    get '/customers/my_page' => 'customers#show'
+    get '/customers/edit' =>'customers#edit'
+    patch '/customers' => 'customers#update'
+    get '/customers/hide' => 'customers#hide'
+    patch '/customers/withdrawal' => 'customers#withdrawal'
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    post '/oders/confirm' => 'orders#confirm'
+    get 'orders/complete' =>'orders#complete'
+  end
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
