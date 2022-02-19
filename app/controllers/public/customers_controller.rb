83e0b2b86b5f8d58cfb505b8e9f56bd9af52f3fb
EC2 Default User <ec2-user@ip-172-31-83-143.ec2.internal>
2022-02-19 09:31:35 +0000

    管理者ページの追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,30 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+    if @customer == current_customer
+      render :edit
+    else
+      redirect_to public_customers_my_page_path
+    end
+  end
+
+  def update
+    @customer = current_customer
+    if @customer == current_customer
+      flash[:notice] = "You have updates user successfully."
+      redirect_to public_customers_my_page_path
+    else
+      render :edit
+    end
   end
 
   def hide
+  end
+
+  def withdrawal
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_history_details, only: [:update]
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
